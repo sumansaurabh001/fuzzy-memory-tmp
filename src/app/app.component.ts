86adff37af635eb4e25235245f56079be5fb6959
JHades <jhades.dev@gmail.com>
2018-03-21 10:18:42 +0100

    ongoing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSidenavContent} from '@angular/material';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private router:Router) {
+
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        tap(() => this.scrollToTop())
+      )
+      .subscribe();
+  }
+
+  scrollToTop() {
+
+    const main = document.querySelector('mat-sidenav-content');
+
+    if (main) {
+      main.scrollTo(0,0);
+    }
+  }
+
+
 }
