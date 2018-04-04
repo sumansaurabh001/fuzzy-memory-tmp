0b86f2a020d6da4c07a3130e86e69c2e139249a1
JHades <jhades.dev@gmail.com>
2018-04-04 16:30:37 +0200

    ongoing

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'video-player',
@@ -11,9 +11,24 @@ export class VideoPlayerComponent implements OnInit {
   @Input()
   url:string;
 
+  @ViewChild('video')
+  videoElement: ElementRef;
+
+
+  videoHover = false;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  get video(): HTMLVideoElement {
+    return this.videoElement ? this.videoElement.nativeElement : undefined;
+  }
+
+  play() {
+    this.video.play();
   }
 
 }
