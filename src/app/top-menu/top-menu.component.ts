3fb2032b498c0eb20def6c673d3b3e22a63ab100
JHades <jhades.dev@gmail.com>
2018-04-18 09:56:46 +0200

    attempt to multi-tenancy solution

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -6,6 +6,7 @@ import {isLoggedIn, isLoggedOut, userPictureUrl} from '../store/selectors';
 import {Logout} from '../store/auth.actions';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {Router} from '@angular/router';
+import {LoadingService} from '../services/loading.service';
 
 @Component({
   selector: 'top-menu',
@@ -22,7 +23,8 @@ export class TopMenuComponent implements OnInit {
   constructor(
     private store: Store<AppState>,
     private afAuth: AngularFireAuth,
-    private router: Router) {
+    private router: Router,
+    private loading: LoadingService) {
 
   }
 
@@ -40,6 +42,7 @@ export class TopMenuComponent implements OnInit {
     this.afAuth.auth.signOut();
     this.store.dispatch(new Logout());
     this.router.navigateByUrl('/courses');
+    this.loading.loadingOn();
     window.location.reload();
   }
 
