ea7d66cc674c68a5e3148fa659a3406aaa6b2597
JHades <jhades.dev@gmail.com>
2019-09-11 11:06:58 +0200

    ongoing

--- functions/firebase-dev-env.sh
+++ functions/firebase-dev-env.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-firebase functions:config:set stripe.secret_key="sk_test_v08Em0hvszSrnn3s7SouTMAL" stripe.public_key="pk_test_WSBTkv7IREB6kOesKAnoUq1Q"  stripe.application_fee_percent="1"  stripe.fulfillment_webhook_secret="whsec_jLrC1wc3AFzIB7TWQsCvTksECrEy8vxa" platform.multi_tenant_mode="on"  platform.mailgun_api_key="key-6972d219a458f74f74fce322f259bea0"
+##
+## currently no environment vars are needed on firebase functions, everything was moved to the REST API
+##
+
+## firebase functions:config:set platform.mailgun_api_key="key-6972d219a458f74f74fce322f259bea0"
+
+
 
 # restart functions only, in order to apply the new environment variables
 firebase deploy --only functions
--- functions/src/api-utils.ts
+++
@@ -1,6 +0,0 @@
-
-
-export function apiError(res, errorDescription) {
-  console.log(errorDescription);
-  res.status(500).json({errorDescription});
-}
