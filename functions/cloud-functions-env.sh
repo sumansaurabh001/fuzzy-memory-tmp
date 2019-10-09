39bf8199b66cfe6cd17dcfaeb2e7ff2fbf77fdcd
JHades <jhades.dev@gmail.com>
2019-10-09 13:16:52 +0200

    ongoing

--- functions/cloud-functions-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-
-
-# needed for creating custom JWTs
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/vasco/vasco/code/fuzzy-memory-tmp/rest-api/service-accounts/onlinecoursehost-local-dev-51579d6b8d0b.json
-
-
