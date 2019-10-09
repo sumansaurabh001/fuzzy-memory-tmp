f9e7c5386f667a4f40dc1b241b6fb7e743a3cb6d
JHades <jhades.dev@gmail.com>
2019-10-09 13:12:32 +0200

    ongoing

--- src/app/services/url-builder.service.ts
+++ src/app/services/url-builder.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
 import {TenantService} from './tenant.service';
 import {Course} from '../models/course.model';
-import {EMPTY_IMG} from '../common/ui-constants';
 import {Lesson} from '../models/lesson.model';
 import {VideoAccess} from '../models/video-access.model';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +16,7 @@ export class UrlBuilderService {
 
   buildCourseThumbnailUrl(course: Course) {
     if (course && course.thumbnail) {
-      return 'https://firebasestorage.googleapis.com/v0/b/onlinecoursehost-local-dev.appspot.com/o/'
-        + this.tenant.id + '%2F' + course.id + '%2Fthumbnail%2F' + course.thumbnail + '?alt=media';
+      return `https://firebasestorage.googleapis.com/v0/b/${environment.firebase.storageBucket}/o/${this.tenant.id}%2F${course.id}%2Fthumbnail%2F${course.thumbnail}?alt=media`;
     }
     else {
       return null;
@@ -28,8 +27,7 @@ export class UrlBuilderService {
 
   buildLessonThumbailUrl(course: Course, lesson:Lesson) {
     if (course && lesson && lesson.thumbnail) {
-      return 'https://firebasestorage.googleapis.com/v0/b/onlinecoursehost-local-dev.appspot.com/o/'
-        + this.tenant.id + '%2F' + course.id + '%2Fvideos%2F' + lesson.id + '%2F' + lesson.thumbnail + '?alt=media';
+      return `https://firebasestorage.googleapis.com/v0/b/${environment.firebase.storageBucket}/o/${this.tenant.id}%2F${course.id}%2Fvideos%2F${lesson.id}%2F${lesson.thumbnail}?alt=media`;
     }
     else {
       return null;
@@ -38,8 +36,7 @@ export class UrlBuilderService {
 
   buildLessonVideoUrl(course: Course, lesson:Lesson, access: VideoAccess) {
     if (course && lesson && lesson.originalFileName) {
-      return 'https://firebasestorage.googleapis.com/v0/b/onlinecoursehost-local-dev.appspot.com/o/'
-        + this.tenant.id + '%2F' + course.id + '%2Fvideos%2F' + lesson.id + '%2F' + access.secretVideoName + '?alt=media';
+      return `https://firebasestorage.googleapis.com/v0/b/${environment.firebase.storageBucket}/o/${this.tenant.id}%2F${course.id}%2Fvideos%2F${lesson.id}%2F${access.secretVideoName}?alt=media`;
     }
     else {
       return null;
