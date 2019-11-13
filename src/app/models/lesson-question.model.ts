f0c4a8a662d5cf1d55599c47b08db1d7a7c5d18b
JHades <jhades.dev@gmail.com>
2019-11-13 11:55:45 +0100

    ongoing

--- src/app/models/lesson-question.model.ts
+++ src/app/models/lesson-question.model.ts
@@ -8,14 +8,5 @@ export interface LessonQuestion extends Question {
 
 
 export function compareQuestions(q1: LessonQuestion, q2:LessonQuestion) {
-
-  const compareCourses = q1.courseId.localeCompare(q2.courseId);
-
-  if (compareCourses!== 0) return compareCourses;
-
-  const compareLessons = q1.lessonId.localeCompare(q2.lessonId);
-
-  if (compareLessons !== 0) return compareLessons;
-
   return q2.createdAt.toMillis() - q1.createdAt.toMillis();
 }
